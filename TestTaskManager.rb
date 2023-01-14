2ae3453a6e007719c95a96af56501056b7b6f850
hidenorly <twitte.harold@gmail.com>
2023-01-15 00:19:54 +0900

    Add test case for ThreadPool

--- TestTaskManager.rb
+++ TestTaskManager.rb
@@ -13,21 +13,6 @@ class TestTask < TaskAsync
 	end
 end
 
-class ResultCollector
-	def initialize
-		@result = {}
-		@_mutex = Mutex.new
-	end
-	def onResult( id, result )
-		@_mutex.synchronize {
-			@result[ id ] = result
-		}
-	end
-	def getResult
-		return @result
-	end
-end
-
 
 class TestTaskManager < Minitest::Test
 	def setup
@@ -40,6 +25,22 @@ class TestTaskManager < Minitest::Test
 		puts "test_TaskManager"
 		resultCollector = ResultCollector.new()
 		taskMan = TaskManagerAsync.new()
+		taskMan.addTask( TestTask.new( "thread1", resultCollector ) )
+		taskMan.addTask( TestTask.new( "thread2", resultCollector ) )
+		taskMan.addTask( TestTask.new( "thread3", resultCollector ) )
+		taskMan.addTask( TestTask.new( "thread4", resultCollector ) )
+
+		taskMan.executeAll()
+		taskMan.finalize()
+
+		result = resultCollector.getResult()
+		assert_equal 4, result.length
+	end
+
+	def test_ThreadPool
+		puts "test_ThreadPool"
+		resultCollector = ResultCollector.new()
+		taskMan = ThreadPool.new()
 		taskMan.addTask( TestTask.new( "thread1", resultCollector ) )
 		taskMan.addTask( TestTask.new( "thread2", resultCollector ) )
 		taskMan.addTask( TestTask.new( "thread3", resultCollector ) )
--- sample.rb
+++ sample.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-#  Copyright (C) 2021 hidenorly
+#  Copyright (C) 2021, 2022 hidenorly
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
